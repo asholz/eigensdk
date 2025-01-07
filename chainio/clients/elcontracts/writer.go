6dbf073f5297cdf2da822be57f78bd389e8c8ab9
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-07 17:31:20 -0300

    chore: remove duplicated function
    `ForceDeregisterFromOperatorSets` does the same as `DeregisterFromOperatorSets`, so we can simply remove it.

--- chainio/clients/elcontracts/writer.go
+++ chainio/clients/elcontracts/writer.go
@@ -452,43 +452,6 @@ func (w *ChainWriter) ProcessClaims(
 	return receipt, nil
 }
 
-func (w *ChainWriter) ForceDeregisterFromOperatorSets(
-	ctx context.Context,
-	operator gethcommon.Address,
-	avs gethcommon.Address,
-	operatorSetIds []uint32,
-	waitForReceipt bool,
-) (*gethtypes.Receipt, error) {
-	if w.allocationManager == nil {
-		return nil, errors.New("AVSDirectory contract not provided")
-	}
-
-	noSendTxOpts, err := w.txMgr.GetNoSendTxOpts()
-	if err != nil {
-		return nil, utils.WrapError("failed to get no send tx opts", err)
-	}
-
-	tx, err := w.allocationManager.DeregisterFromOperatorSets(
-		noSendTxOpts,
-		allocationmanager.IAllocationManagerTypesDeregisterParams{
-			Operator:       operator,
-			Avs:            avs,
-			OperatorSetIds: operatorSetIds,
-		},
-	)
-
-	if err != nil {
-		return nil, utils.WrapError("failed to create ForceDeregisterFromOperatorSets tx", err)
-	}
-
-	receipt, err := w.txMgr.Send(ctx, tx, waitForReceipt)
-	if err != nil {
-		return nil, utils.WrapError("failed to send tx", err)
-	}
-
-	return receipt, nil
-}
-
 func (w *ChainWriter) ModifyAllocations(
 	ctx context.Context,
 	operatorAddress gethcommon.Address,
