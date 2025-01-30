ad36295a66a358e2ad368a0ece574525a4fbd8cf
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-01-30 12:14:40 -0300

    chore: revert license change on contract

--- contracts/src/ContractsRegistry.sol
+++ contracts/src/ContractsRegistry.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
 // ContractsRegistry store the address of all the contracts deployed for eigenlayer and avss
