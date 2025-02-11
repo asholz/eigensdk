0789e4f2aeafc98fc00d2df5b6e9f96f24c3f453
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-02-11 13:30:25 -0300

    test: add test for new function

--- chainio/clients/avsregistry/writer_test.go
+++ chainio/clients/avsregistry/writer_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients/avsregistry"
 	chainioutils "github.com/Layr-Labs/eigensdk-go/chainio/utils"
+	avsdirectory "github.com/Layr-Labs/eigensdk-go/contracts/bindings/AVSDirectory"
 	regcoord "github.com/Layr-Labs/eigensdk-go/contracts/bindings/RegistryCoordinator"
 	servicemanager "github.com/Layr-Labs/eigensdk-go/contracts/bindings/ServiceManagerBase"
 	stakeregistry "github.com/Layr-Labs/eigensdk-go/contracts/bindings/StakeRegistry"
@@ -636,4 +637,32 @@ func TestSetEjectionCooldown(t *testing.T) {
 	newCooldown, err := registryCoordinatorContract.EjectionCooldown(&bind.CallOpts{})
 	require.NoError(t, err)
 	assert.Equal(t, newCooldown, ejectionCooldown)
+}
+
+func TestUpdateAVSMetadataURI(t *testing.T) {
+	clients, _ := testclients.BuildTestClients(t)
+	chainWriter := clients.AvsRegistryChainWriter
+
+	svcManagerAddr := clients.AvsRegistryContractBindings.ServiceManagerAddr
+	avsDirectoryAddr := clients.EigenlayerContractBindings.AvsDirectoryAddr
+
+	avsDirectory, err := avsdirectory.NewContractAVSDirectory(
+		avsDirectoryAddr,
+		clients.EthHttpClient,
+	)
+	require.NoError(t, err)
+
+	// Prepare event filter to check for URI update
+	iter, err := avsDirectory.FilterAVSMetadataURIUpdated(nil, []gethcommon.Address{svcManagerAddr})
+	require.NoError(t, err)
+
+	// Update the metadata URI
+	newMetadata := "https://new-metadata-uri.com"
+	receipt, err := chainWriter.UpdateAVSMetadataURI(context.TODO(), newMetadata, true)
+	require.NoError(t, err)
+	require.Equal(t, gethtypes.ReceiptStatusSuccessful, receipt.Status)
+
+	// Assert the event was emitted
+	require.True(t, iter.Next())
+	require.Equal(t, newMetadata, iter.Event.MetadataURI)
 }
