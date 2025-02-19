a8782ebfb58a69bf27cff744f323fae248456422
Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>
2025-02-19 15:23:22 -0300

    Update services/bls_aggregation/blsagg.go
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- services/bls_aggregation/blsagg.go
+++ services/bls_aggregation/blsagg.go
@@ -145,7 +145,7 @@ func (t TaskMetadata) WithWindowDuration(windowDuration time.Duration) TaskMetad
 // BlsAggregationService is the interface provided to avs aggregator code for doing bls aggregation
 // Currently its only implementation is the BlsAggregatorService, so see the comment there for more details
 type BlsAggregationService interface {
-	// Starts the aggregation service, and returns the handler, to interact with, it and the response channel,
+	// Starts the aggregation service, returning a handler to interact with it and a response channel
 	// to receive the aggregated responses.
 	Start() (ServiceHandler, <-chan BlsAggregationServiceResponse)
 }
